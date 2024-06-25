abc5ef4e4a936746eb2de23b87bb700787a37c00
Conan O'Reilly <conanoreilly20@gmail.com>
2024-06-25 10:56:03 +0100

    Import libraries

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib as plt
+
+df = pd.read_csv(r"C:\Users\conan\OneDrive\Documents\Data Analysis Project\Datasets\Fuels Used in Electricity Production.csv")
