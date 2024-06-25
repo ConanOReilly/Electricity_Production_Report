3d79a8714993738957f2a4578194b30f9852d306
Conan O'Reilly <conanoreilly20@gmail.com>
2024-06-25 11:13:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import seaborn as sns
 import matplotlib as plt
 
 df = pd.read_csv(r"C:\Users\conan\OneDrive\Documents\Data Analysis Project\Datasets\Fuels Used in Electricity Production.csv")
+
+print(df)
+
+df.info()
